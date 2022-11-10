461fa7ae316099da7ab5f50d16801da6a08fb2c0
Christopher Waters <chris.isaac.waters@gmail.com>
2022-11-10 16:09:09 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+namespace censored
+{
+    class Program
+    {
+        static String censor(String text,
+                      List<string> words)
+        {
+
+            foreach (var word in words)
+            {
+
+                // Creating the censor which is an asterisks
+                // "*" text of the length of censor word
+                String stars = "";
+                for (int i = 0; i < word.Length; i++)
+                    stars += '*';
+
+                text = text.Replace(word, stars);
+
+            }
+
+            return text;
+        }
+
+        public static void Main(String[] args)
+        {
+            string extract = System.IO.File.ReadAllText(@"input.txt");
+
+            var censoredFile = System.IO.File.ReadAllText(@"censored.txt");
+
+            var pattern = @"""(?<r>[^""]*)""|'(?<r>[^']*)'|(?<r>[^\s,]+)";
+            var censoredwords = Regex.Matches(censoredFile, pattern).Cast<Match>().Select(x => x.Groups["r"].Value).ToList();
+
+            File.WriteAllText("output.txt",(censor(extract, censoredwords)));
+
+            Console.WriteLine("New censored text written to output.txt");
+
+        }
+
+    }
+}
